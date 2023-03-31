93b3d7535b2d96c33bf683b63d68a50a7d147df3
Piotr Lemańczyk <72319172+Lemanczyk0Piotr@users.noreply.github.com>
2023-03-31 23:00:02 +0200

    Create JSZYCER.cpp

---
+++ ŁATWE/JSZYCER.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string text;
+    string alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    while(getline(cin, text))
+    {
+        for (int i=0; i<text.length(); i++)
+        {
+            for(int j=0; j<alphabet.length(); j++)
+            {
+                if(text[i]==' ')
+                {
+                    cout<<" ";
+                    break;
+                }
+                else if(text[i] == alphabet[j])
+                {
+                    if(j>=23)
+                    {
+                        cout<<alphabet[0+(j-23)];
+                        break;
+                    }
+                    else
+                    {
+                        cout<<alphabet[j+3];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
