d9a89e2008d4d7ba738720e5a1544aa10f6d5df5
Piotr Lemańczyk <72319172+Lemanczyk0Piotr@users.noreply.github.com>
2023-04-03 08:14:00 +0200

    Update FZI_STEF.cpp

--- ŁATWE/FZI_STEF.cpp
+++ ŁATWE/FZI_STEF.cpp
@@ -4,27 +4,26 @@ using namespace std;
   
 int main() {
     int miasta, kaska;
-    long long suma=0, maks=0; //long long - nie wiemy ile mozna zarobic nawet po kilkadziesiąt miliardów, int by miał za malo znków
+    long long suma=0, maks=0; 
+    cin >> miasta; 
 	
-    cin >> miasta; // wprowadzamy ilość miast
-	
-    while (miasta--) //odejmujemy jeden po kazdym przejsciu calej pętli az dojdzie do 0
+    while (miasta--)
     {
-            cin >> kaska; //ile zarobi na jednym
+            cin >> kaska;
   
-            suma+=kaska; // to samo co suma = suma + kaska
+            suma+=kaska; 
   
-            if (suma>maks) // wszystko jasne po dodaniu 
+            if (suma>maks)
 	    {
-                maks=suma; // jezeli jest suma wieksza to przypisujemy bo pewnie bedzie to maks nasza wartość
+                maks=suma; 
             }
-            if (suma<0) // wszystko jasne xd bo zarobek chyba nie moze byc ujemny
+            if (suma<0) 
 	    {
-                suma=0; // wszystko jasne, w sensie moze byc ujemny ale nie ma takije mozliwosci w zadaniu, w jedmyn przykladiz jest ze jak masz mniej niz 0 to ma zawsze wyswitlic 0
+                suma=0; 
             }
     }
   
-        cout << maks << endl; //wyjscie danych
+        cout << maks << endl; 
   
     return 0;
   
